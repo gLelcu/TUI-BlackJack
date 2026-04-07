558f286610dff866ec245358aea4bfb6adc22ff1
Lelcu Gabriel <gabilelcu26@gmail.com>
2026-04-07 23:58:01 +0300

    Refactor blackjack.cpp for improved readability

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,5 +1,5 @@
-/// in caz ca uit sa compilez
-/// g++ blackjack.cpp -lncurses -lm -o blackjack
+
+/// a se compila cu:  `g++ blackjack.cpp -lncurses -lm -o blackjack`
 
 #include <iostream>
 #include <cmath>
@@ -33,6 +33,7 @@ void buildWindow(WINDOW *win)
     mvprintw(4, 63, "s - stay");
     mvprintw(5, 63, "d - double");
     mvprintw(6, 63, "p - split");
+    mvprintw(7, 63, "q - quit");
 
     refresh();
     wrefresh(win); // refresh sa vedem cutia  
@@ -48,48 +49,76 @@ void dealcard(WINDOW *win, int x, int y, int r, int s) {
     mvwprintw(win, y + 6, x, "+--------+");
 }
 
-void first2cardsdealer(WINDOW *win){
+void first2cardsdealer(WINDOW *win){ //primele 2 carti si calculam scorul
     int rank = rand() % 13;
     dealcard(win, 1, 1, rank, rand() % 4);
+    if(rank == 0)
+            if(score.dealer + 11 > 21)
+                score.dealer += (rank >= 9) ? 10 : rank + 1;
+            else   score.dealer += 11; //daca scorul cu 11 depaseste 21 asul e 1 si viceversa
+    else
+        score.dealer += (rank >= 9) ? 10 : rank + 1;  // adunam la scor valoarea cartii si daca e mai mare ca 9 adunam 10
+        
     refresh();
-    score.dealer += (rank >= 9) ? 10 : rank + 1; // adunam la score valoare si daca e mai mare ca 9 adunam 10
     wrefresh(win);
     rank = rand() % 13;
     dealcard(win, 12, 1, rank, rand() % 4);
-    score.dealer += (rank >= 9) ? 10 : rank + 1;
+    if(rank == 0)
+            if(score.dealer + 11 > 21)
+                score.dealer += (rank >= 9) ? 10 : rank + 1;
+            else score.dealer += 11;
+    else
+        score.dealer += (rank >= 9) ? 10 : rank + 1;
     refresh();
     wrefresh(win);
 }
 
-void first2cardsplayer(WINDOW *win){
+void first2cardsplayer(WINDOW *win){ // facem acelasi lucru
     int rank = rand() % 13;
     dealcard(win, 1, 11, rank, rand() % 4);
-    score.player += (rank >= 9) ? 10 : rank + 1; // la fel si pt player
+    if(rank == 0)
+            if(score.player + 11 > 21)
+                score.player += (rank >= 9) ? 10 : rank + 1;
+            else   score.player += 11;
+    else
+        score.player += (rank >= 9) ? 10 : rank + 1;
     refresh();
     wrefresh(win);
     rank = rand() % 13;
     dealcard(win, 12, 11, rank, rand() % 4);
-    score.player += (rank >= 9) ? 10 : rank + 1;
+    if(rank == 0)
+            if(score.player + 11 > 21)
+                score.player += (rank >= 9) ? 10 : rank + 1;
+            else   score.player += 11;
+    else
+        score.player += (rank >= 9) ? 10 : rank + 1;
     refresh();
     wrefresh(win);
 }
 void dealdealer(WINDOW *win, int &x){
 
     if(score.dealer == 21){
+        wattron(win, COLOR_PAIR(1));
         mvwprintw(win, 0, 20, "  BLACKJACK    ");
+        wattroff(win, COLOR_PAIR(1));
         refresh();
         wrefresh(win);
     }
-    if (score.dealer > 21) {
+    else if (score.dealer > 21) {
         mvwprintw(win, 0, 20, "  DEALER BUST    ");
         refresh();
         wrefresh(win);
     }
-    if(score.dealer < 17) {
+    else if(score.dealer < 17) {
         x += 11;
         int rank = rand() % 13;
         dealcard(win, x, 1, rank, rand() % 4);
-        score.dealer += (rank >= 9) ? 10 : rank + 1;
+        if(rank == 0)
+            if(score.dealer + 11 > 21)
+                score.dealer += (rank >= 9) ? 10 : rank + 1;
+            else   score.dealer += 11;
+        else
+            score.dealer += (rank >= 9) ? 10 : rank + 1;
         refresh();
         wrefresh(win);
         dealdealer(win, x);
@@ -97,8 +126,14 @@ void dealdealer(WINDOW *win, int &x){
 }
 void dealplayer(WINDOW *win, int &y){
     int rank = rand() % 13;
-    dealcard(win, y+11, 11, rank, rand() % 4);
-    score.player += (rank >= 9) ? 10 : rank + 1;
+     y+=11;
+    dealcard(win, y, 11, rank, rand() % 4);
+    if(rank == 0)
+            if(score.player + 11 > 21)
+                score.player += (rank >= 9) ? 10 : rank + 1;
+            else   score.player += 11;
+    else
+        score.player += (rank >= 9) ? 10 : rank + 1;
     refresh();
     wrefresh(win);
 }
@@ -114,6 +149,11 @@ int endgame(WINDOW *win, int &x, int &y){
             score.player = 0;
             werase(win);
             buildWindow(win);
+            first2cardsdealer(win);
+            first2cardsplayer(win);
+            mvwprintw(win, 8, 63, "ANOTHER ONE!"); 
+            refresh();
+            wrefresh(win);
             x = 12;
             y = 12;
             return 1;
@@ -127,76 +167,88 @@ int endgame(WINDOW *win, int &x, int &y){
          return 0;
 }
 
+bool checkscore(WINDOW *win, bool finalCompare){
+    if(score.player > 21){
+        mvwprintw(win, 0, 20, "  PLAYER BUST, DEALER WINS    ");
+        refresh();
+        wrefresh(win);
+        return true;
+    }
+    if(score.dealer > 21){
+        mvwprintw(win, 0, 20, "  PLAYER WINS, DEALER BUST    ");
+        refresh();
+        wrefresh(win);
+        return true;
+    }
+
+    if(!finalCompare){
+        if(score.player == 21){
+            wattron(win, COLOR_PAIR(1));
+            mvwprintw(win, 0, 20, "  BLACKJACK    ");
+            wattroff(win, COLOR_PAIR(1));
+            refresh();
+            wrefresh(win);
+            return true;
+        }
+        return false;
+    }
+
+    if(score.player == score.dealer){
+        mvwprintw(win, 0, 20, "  PUSH    ");
+    }
+    else if(score.dealer > score.player){
+        mvwprintw(win, 0, 20, "  DEALER WINS    ");
+    }
+    else {
+        mvwprintw(win, 0, 20, "  PLAYER WINS    ");
+    }
+
+    refresh();
+    wrefresh(win);
+    return true;
+}
+
 int main(){
 
     initscr();//mandatoriu 
     noecho();//nu afiseaza ce scriem
     curs_set(0);//ascunde cursorul
     srand(time(0));//seed pt random
+    start_color();
+    use_default_colors();
+    init_pair(1, COLOR_RED, -1);
 
     WINDOW *win = newwin(20, 60, 1, 1); // noua fereastra
     buildWindow(win);    
     
     int x = 12, y = 12;
+    first2cardsdealer(win);
+    first2cardsplayer(win);
+
     while (true){
-        
-        first2cardsdealer(win);
-        first2cardsplayer(win);
+               
         char ply = wgetch(win);//player choice
         if(ply == 'h') {
             dealplayer(win, y);
-            if(score.player > 21) {
-                mvwprintw(win, 0, 20, "  PLAYER BUST    ");
-                refresh();
-                wrefresh(win);
-                if (!endgame(win, x, y)) {
-                    break;
-                }
-            }
-            else if (score.player == 21) {
-                mvwprintw(win, 0, 20, "  BLACKJACK    ");
-                refresh();
-                wrefresh(win);
-                if (!endgame(win, x, y)) {
-                    break;
-                }
+            if(checkscore(win, false) && !endgame(win, x, y)) {
+                break;
             }
         }
         else if (ply == 's'){
             dealdealer(win, x);
-            if(score.dealer == score.player) {
-                mvwprintw(win, 0, 20, "  PUSH    ");
-                refresh();
-                wrefresh(win);
-                if (!endgame(win, x, y)) {
-                    break;
-                }
+            if(checkscore(win, true) && !endgame(win, x, y)) {
+                break;
             }
-            else if(score.dealer > score.player && score.dealer <= 21) {
-                mvwprintw(win, 0, 20, "  DEALER WINS    ");
-                refresh();
-                wrefresh(win);
-                if (!endgame(win, x, y)) {
-                    break;
-                }
-            }
-            else {
-                mvwprintw(win, 0, 20, "  PLAYER WINS    ");
-                refresh();
-                wrefresh(win);
-                if (!endgame(win, x, y)) {
-                    break;
-                }
-            }
+        }
+        else if(ply == 'q'){
+            break;
         }
         else {
-            mvwprintw(win, 0, 20, "  INVALID INPUT    ");
+            mvwprintw(win, 8, 63, "  INVALID INPUT    ");
             refresh();
             wrefresh(win);
-            if(!endgame(win, x, y)) {
-                break;
         }
-        }
+
     }
     getch(); //asteapta o tasta    
     endwin(); //mandatoriu
